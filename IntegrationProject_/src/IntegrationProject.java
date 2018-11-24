32bb6317c89d7d69100e9f257c3388d64a2041e4
MontoyaR <42822862+MontoyaR@users.noreply.github.com>
2018-11-24 16:42:29 -0500

    Integration Project Ready

--- IntegrationProject_/src/IntegrationProject.java
+++ IntegrationProject_/src/IntegrationProject.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -46,156 +47,154 @@ public class IntegrationProject {
 		System.out.println("\t 32. Enhanced For Loop [ ArrayList ] ");
 		System.out.println("\t 33. Multi-Dimensioal Array [ 2-Dimensional Array ]");
 
+		Scanner scan = new Scanner(System.in);
+
 		int selection;
 
-		Scanner scan = new Scanner(System.in);
-try {
-	
-}
-catch (ExceptionType ex) {
-	
-}
-		selection = scan.nextInt();
+		try {
+			selection = scan.nextInt();
 
-		switch (selection) {
-		case 1:
-			Module2List.selection1();
-			break;
+			switch (selection) {
+			case 1:
+				Module2List.selection1();
+				break;
 
-		case 2:
-			Module2Integer.selection2();
-			break;
+			case 2:
+				Module2Integer.selection2();
+				break;
 
-		case 3:
-			Module2Double.selection3();
-			break;
+			case 3:
+				Module2Double.selection3();
+				break;
 
-		case 4:
-			Module2String.selection4();
-			break;
+			case 4:
+				Module2String.selection4();
+				break;
 
-		case 5:
-			Module2Boolean.selection5();
-			break;
+			case 5:
+				Module2Boolean.selection5();
+				break;
 
-		case 6:
-			Module2Definition.selection6();
-			break;
+			case 6:
+				Module2Definition.selection6();
+				break;
 
-		case 7:
-			Module2Final.selection7();
-			break;
+			case 7:
+				Module2Final.selection7();
+				break;
 
-		case 8:
-			Module3Casting.selection8();
-			break;
+			case 8:
+				Module3Casting.selection8();
+				break;
 
-		case 9:
-			Module3MethodOne.selection9();
-			break;
+			case 9:
+				Module3MethodOne.selection9();
+				break;
 
-		case 10:
-			Module3MethodTwo.selection10();
-			break;
+			case 10:
+				Module3MethodTwo.selection10();
+				break;
 
-		case 11:
-			Module3MethodThree.selection11();
-			break;
+			case 11:
+				Module3MethodThree.selection11();
+				break;
 
-		case 12:
-			Module3EscapeSequence.selection12();
-			break;
+			case 12:
+				Module3EscapeSequence.selection12();
+				break;
 
-		case 13:
-			Module5RandomGenerator.selection13();
-			break;
+			case 13:
+				Module5RandomGenerator.selection13();
+				break;
 
-		case 14:
-			Module5MathClass.selection14();
-			break;
+			case 14:
+				Module5MathClass.selection14();
+				break;
 
-		case 15:
-			Module6IfElse.selection15();
-			break;
+			case 15:
+				Module6IfElse.selection15();
+				break;
 
-		case 16:
-			Module6If.selection16();
-			break;
+			case 16:
+				Module6If.selection16();
+				break;
 
-		case 17:
-			Module6Switch.selection17();
-			break;
+			case 17:
+				Module6Switch.selection17();
+				break;
 
-		case 18:
-			Module6CompareString.selection18();
-			break;
+			case 18:
+				Module6CompareString.selection18();
+				break;
 
-		case 19:
-			Module6EqualsMethod.selection19();
-			break;
+			case 19:
+				Module6EqualsMethod.selection19();
+				break;
 
-		case 20:
-			Module6Operators.selection20();
-			break;
+			case 20:
+				Module6Operators.selection20();
+				break;
 
-		case 21:
-			Module7WhileLoop.selection21();
-			break;
+			case 21:
+				Module7WhileLoop.selection21();
+				break;
 
-		case 22:
-			Module7ForLoop.selection22();
-			break;
+			case 22:
+				Module7ForLoop.selection22();
+				break;
 
-		case 23:
-			Module7DoWhileLoop.selection23();
-			break;
+			case 23:
+				Module7DoWhileLoop.selection23();
+				break;
 
-		case 24:
-			Module7BreakContinueLoop.selection24();
-			break;
+			case 24:
+				Module7BreakContinueLoop.selection24();
+				break;
 
-		case 25:
-			Module8OOP.selection25();
-			break;
-		case 26:
-			Module9Overloading.selection26();
-			break;
+			case 25:
+				Module8OOP.selection25();
+				break;
+			case 26:
+				Module9Overloading.selection26();
+				break;
 
-		case 27:
-			Module9Inheritance.selection27();
-			break;
+			case 27:
+				Module9Inheritance.selection27();
+				break;
 
-		case 28:
-			Module9Polymorphism.selection28();
-			break;
+			case 28:
+				Module9Polymorphism.selection28();
+				break;
 
-		case 29:
-			Module9Super.selection29();
-			break;
+			case 29:
+				Module9Super.selection29();
+				break;
 
-		case 30:
-			Module9This object = new Module9This(10, 20);
-			object.display();
-			break;
+			case 30:
+				Module9This object = new Module9This(10, 20);
+				object.display();
+				break;
 
-		case 31:
-			Module10Array1D.selection31();
-			break;
+			case 31:
+				Module10Array1D.selection31();
+				break;
 
-		case 32:
-			Module10EnhancedLoop.selection32();
-			Module10EnhancedLoop.enhancedForLoop();
-			break;
+			case 32:
+				Module10EnhancedLoop.selection32();
+				Module10EnhancedLoop.enhancedForLoop();
+				break;
 
-		case 33:
-			Module10MultiDimensionalArray.selection33();
-			break;
-		
-		default:
-			System.out.println("Invalid selection");
+			case 33:
+				Module10MultiDimensionalArray.selection33();
+				break;
 
+			default:
+				System.out.println("Invalid selection");
+
+			}
+		} catch (Exception e) {
+			System.out.println("Invalid Input ");
+			System.out.println("Exception is: " + e);
 		}
-		scan.close();
 	}
-
 }
